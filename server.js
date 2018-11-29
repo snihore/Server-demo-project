2aeaac329904c8effb0f803bb3c5c485a46c44a2
Sourabh Nihore <souravnihore1998@gmail.com>
2018-11-29 19:10:19 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+console.log('Hello Node Server');
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', (req, res)=>{
+
+    // res.send('Hello MERE User');
+    res.send({
+        name: 'APKA NAAM',
+        age: 'AGE BATAO BHAI',
+        gender: 'MALE ho ya FEMALE'
+
+    })
+
+});
+
+app.get('/about', (req, res)=>{
+    res.send('<p></p>This is our <strong>about page.</strong></p>');
+});
+
+app.get('/type', (req, res)=>{
+    res.render('template-1.hbs', {
+        name_type: 'Srv123'
+    });
+});
+
+app.listen(3000);
